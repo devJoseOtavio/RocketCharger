aba3cb6738a1a9a398b78969a20ce5678c8a1532
jose <jose.gallo@asaas.com.br>
2022-04-25 10:24:42 -0300

    criando formData

--- grails-app/assets/javascripts/newPostalCode-api.js
+++ grails-app/assets/javascripts/newPostalCode-api.js
@@ -1,28 +1,29 @@
-document.getElementById("postalCode").addEventListener("input", checkAddress);
+function getCep() {
+  let postalCode = document.getElementById('postalCode').value
 
-function checkAddress() {
-  var postalCode = document.querySelector("#postalCode").value;
-  let correctPostalCodeLength = 8;
-  if (postalCode.length == correctPostalCodeLength) {
-    let url = `https://viacep.com.br/ws/${postalCode}/json/`;
-
-    fetch(url).then(function (response) {
-      response.json().then(showAddress);
-    });
-  }
+  fetch(`https://viacep.com.br/ws/${postalCode}/json`).then(result => result.json()) 
+  .then(data => { 
+      if (!("erro" in data)) {
+          document.querySelector('#address').value = data.logradouro
+          document.querySelector('#province').value = data.bairro
+          document.querySelector('#city').value = data.localidade
+          document.querySelector('#state').value = data.uf
+      } else {
+          alert("CEP não encontrado");
+      }
+   })
 }
 
-function showAddress(data) {
-  let address = document.querySelector("#address");
-  let province = document.querySelector("#province");
-  let state = document.querySelector("#state");
-  let city = document.querySelector("#city");
+document.getElementById('postalCode').addEventListener('focusout', getCep);
 
-  if (!data.erro) {
-    address.value = `${data.logradouro}`;
-    province.value = `${data.bairro}`;
-    state.value = `${data.uf}`;
-    city.value = `${data.localidade}`;
-    return;
-  }
-}
+document.querySelector("#form").addEventListener("submit", function(event) {
+  event.preventDefault();
+  
+  var customer = {}
+  var form = document.querySelector("form")
+  var data = new FormData(form)
+  data.forEach(function(value, key){
+      customer[key] = value;
+  });  
+})
+
