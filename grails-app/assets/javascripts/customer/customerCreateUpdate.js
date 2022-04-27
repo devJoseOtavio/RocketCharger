2038bbcc5a0a357ee5cfd0de029ac0e10a6a25a7
jose <jose.gallo@asaas.com.br>
2022-04-27 13:52:48 -0300

    organizando nomeclatura

--- grails-app/assets/javascripts/customer/customerCreateUpdate.js
+++ grails-app/assets/javascripts/customer/customerCreateUpdate.js
@@ -3,24 +3,17 @@ $(document).ready(function () {
     e.preventDefault();
 
     var data = new FormData(document.querySelector("form"));
-    var payer = {};
+    var customer = {};
 
     data.forEach(function (value, key) {
-      payer[key] = value;
+      customer[key] = value;
     });
 
     var url = $("form").prop("action");
 
-<<<<<<< HEAD:grails-app/assets/javascripts/customer/customerCreateUpdate.js
     $.post(url, customer, function (response) {
-      console.log(response);
-      if (response.success) {
-        window.location.href = "/customer";
-=======
-    $.post(url, payer, function (response) {
       if (response.success) {
         window.location.href = $("form").data("redirect-url");
->>>>>>> 1fce4c2aad4b0e7358d7fa8d0387741786479bfc:grails-app/assets/javascripts/payer/payerCreateSave.js
         return;
       }
     });
