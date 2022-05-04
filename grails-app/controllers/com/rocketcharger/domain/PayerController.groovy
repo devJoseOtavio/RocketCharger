85dc810efecbc80c355e14772c89162b8efd2297
Luiz Filipe Lima Teston <luizfilipe.liima@gmail.com>
2022-05-04 10:40:04 -0300

    realizado remoção de import não utilizado

--- grails-app/controllers/com/rocketcharger/domain/PayerController.groovy
+++ grails-app/controllers/com/rocketcharger/domain/PayerController.groovy
@@ -3,7 +3,7 @@ package com.rocketcharger.domain
 import com.rocketcharger.domain.payer.Payer
 
 import grails.validation.ValidationException
-import static org.springframework.http.HttpStatus.*
+
 import grails.converters.JSON
 
 class PayerController {
