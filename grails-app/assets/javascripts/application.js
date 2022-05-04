64fff7511b0b81262b361a6f7bd147307856f8fc
Luiz Filipe Lima Teston <luizfilipe.liima@gmail.com>
2022-05-04 15:49:26 -0300

    Correções api viaCep

--- grails-app/assets/javascripts/application.js
+++ grails-app/assets/javascripts/application.js
@@ -1,6 +1,6 @@
 //= require jquery-3.3.1.min
 //= require bootstrap
+//= require utils/CreateSaveController
 //= require utils/loadAddress
 //= require utils/postalCodeApi
-//= require utils/CreateSaveController
 //= require utils/formEdit
--- grails-app/assets/javascripts/utils/loadAddress.js
+++ grails-app/assets/javascripts/utils/loadAddress.js
@@ -1,19 +1,25 @@
-var postalCode = document.getElementById("postalCode");
+$(document).ready(function () {
+  var postalCode = document.getElementById("postalCode");
 
-postalCode.addEventListener("input", function () {
-  if (!postalCode) {
-    return;
-  }
-  validatePostalCode(this.value), getPostalCode(this.value, fillAddress);
-});
+  postalCode.addEventListener("input", function () {
+    if (!postalCode) {
+      return;
+    }
+    validatePostalCode(this.value), getPostalCode(this.value, fillAddress);
+  });
 
-function fillAddress(data) {
-  if (data.erro) {
-    alert(`O cep ${postalCode.value} não foi localizado.`);
-    return;
-  }
-    document.querySelector("#address").value = data.logradouro ? data.logradouro :  "";
+  function fillAddress(data) {
+    if (data.erro) {
+      alert(`O cep ${postalCode.value} não foi localizado.`);
+      return;
+    }
+    document.querySelector("#address").value = data.logradouro
+      ? data.logradouro
+      : "";
     document.querySelector("#district").value = data.bairro ? data.bairro : "";
-    document.querySelector("#city").value = data.localidade ? data.localidade : "";
+    document.querySelector("#city").value = data.localidade
+      ? data.localidade
+      : "";
     document.querySelector("#state").value = data.uf ? data.uf : "";
-}
+  }
+})
