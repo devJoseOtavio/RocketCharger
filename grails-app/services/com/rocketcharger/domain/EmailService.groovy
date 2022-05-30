22da7305952526ba8cd788f90386c7438a549030
Luiz Filipe Lima Teston <luizfilipe.liima@gmail.com>
2022-05-30 09:03:45 -0300

    correção espaçamentos

--- grails-app/services/com/rocketcharger/domain/EmailService.groovy
+++ grails-app/services/com/rocketcharger/domain/EmailService.groovy
@@ -3,7 +3,9 @@ package com.rocketcharger.domain
 import grails.plugin.asyncmail.AsynchronousMailService
 
 class EmailService {
+
     AsynchronousMailService asynchronousMailService
+    
     def sendEmail(String email, String mailSubject, String mailBody) {
         asynchronousMailService.sendMail {
             to email
