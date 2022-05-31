9350b1097dfc5accb0d3a38e6fcc4c881c59a90a
Luiz Filipe Lima Teston <luizfilipe.liima@gmail.com>
2022-05-31 13:27:05 -0300

    .

--- grails-app/services/com/rocketcharger/domain/PaymentService.groovy
+++ grails-app/services/com/rocketcharger/domain/PaymentService.groovy
@@ -3,15 +3,31 @@ package com.rocketcharger.domain
 import com.rocketcharger.domain.payment.Payment
 import com.rocketcharger.domain.payer.Payer
 import com.rocketcharger.domain.customer.Customer
+import com.rocketcharger.enums.PaymentMethod
+import com.rocketcharger.enums.PaymentStatus
+import com.rocketcharger.utils.FormatDateUtils
+
 import grails.gorm.transactions.Transactional 
 
 @Transactional
 class PaymentService {
 
     public Payment save(Map params) {
-        Payment payment = new Payment(params)
-        payment.customer = Customer.get(params.long("customerId"))
+        Payment payment = new Payment()
+        payment.value = new BigDecimal(params.value)
+        payment.dueDate = FormatDateUtils.toDate(params.dueDate, "yyyy-MM-dd")
+        payment.billingType = PaymentMethod.valueOf(params.billingType)
         payment.payer = Payer.get(params.long("payerId"))
+        payment.customer = Customer.get(params.long("customerId"))
+        payment.status = PaymentStatus.PENDING
+        payment.save(failOnError: true)
+        return payment
+    }
+
+    public Payment recognizePayment(paymentId) {
+        Payment payment = Payment.get(paymentId)
+        payment.status = PaymentStatus.PAID
+        payment.paymentDate = new Date()
         payment.save(failOnError: true)
         return payment
     }
@@ -20,16 +36,32 @@ class PaymentService {
         return Payment.getAll()
     }
 
-    public Payment update(Map params){
-        if (!params.id) return
-        Payment payment = Payment.get(params.int("id"))
-        payment.value = params.value
-        payment.billingType = params.billingType
-        payment.status = params.status
-        payment.dueDate = params.dueDate
-        payment.customer = params.customer
-        payment.payer = params.payer
-        payment.save(flush: true, failOnError: true)
-        return payment
+    public Payment getPayment(Long id) {
+        return Payment.get(id)
+    }
+
+    public List<Payment> returnPaymentsByCustomer(Long customerId, Integer max = null, Integer offset = null) {
+        List<Payment> paymentList = Payment.createCriteria().list(max: max, offset: offset){
+            eq("customer", Customer.get(customerId))
+        }
+        return paymentList
+    }
+
+    public List<Payment> list(PaymentStatus paymentStatus, Date yesterdayDate) {
+        List<Payment> paymentList = Payment.createCriteria().list() {
+            eq("status", paymentStatus) and {
+                le("dueDate", yesterdayDate)
+            }
+        }
+        return paymentList
+    }
+
+    public Payment verifyOverDueDates() {
+        Date yesterdayDate = FormatDateUtils.getYesterdayDate()
+        List<Payment> paymentList = list(PaymentStatus.PENDING, yesterdayDate)
+          for(Payment payment : paymentList) {
+              payment.status = PaymentStatus.OVERDUE
+              payment.save(failOnError:true)
+        }
     }
 }
